e0036ccdfe4969dadd2ba70a68556dfe551749f2
ananya-dhiman <ananya.dhiman@somaiya.edu>
2025-02-23 15:56:15 +0530

    Code with Form and Home page

---
+++ Frontend/src/db.js
@@ -0,0 +1,24 @@
+export const user_data=[
+    {
+        id:1,
+        name:"John Doe",
+        email:"john.doe@gmail.com",
+        password:"secret",
+
+
+
+    }
+]
+
+export const roadmap_data=[
+    {
+        id:1,           
+        title:"Mathematics", 
+        userId: 1,      
+        totalNodes:5,      
+        completedNodes:3
+      
+    }
+]
+
+
